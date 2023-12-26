0aaaf32ee4746444b38c1c49b48aecf40539a044
rimuru1412 <gilangarya03@gmail.com>
2023-12-26 18:54:08 +0700

    latest

--- database/seeders/DepartemenSeeder.php
+++ database/seeders/DepartemenSeeder.php
@@ -14,11 +14,67 @@ class DepartemenSeeder extends Seeder
     public function run(): void
     {
         Departemen::create([
-            'nama' => 'Keuangan'
+            'nama' => 'Director'
         ]);
 
         Departemen::create([
-            'nama' => 'Publikasi'
+            'nama' => 'Deputy Director for Administration'
+        ]);
+
+        Departemen::create([
+            'nama' => 'Science Innovation and Technology'
+        ]);
+
+        Departemen::create([
+            'nama' => 'Human Center and Innovation Department'
+        ]);
+
+        Departemen::create([
+            'nama' => 'Finance and Facilities Management Department'
+        ]);
+
+        Departemen::create([
+            'nama' => 'Human Resource and Administration Departement'
+        ]);
+
+        Departemen::create([
+            'nama' => 'Knowledge Management, Publication and Communication Unit'
+        ]);
+
+        Departemen::create([
+            'nama' => 'Earth Observatory and Change Section'
+        ]);
+
+        Departemen::create([
+            'nama' => 'Environmental Technology and Security Section'
+        ]);
+
+        Departemen::create([
+            'nama' => 'Environmental and Finance Risk Management Section'
+        ]);
+
+        Departemen::create([
+            'nama' => 'Environmental Policy and Governance Section'
+        ]);
+
+        Departemen::create([
+            'nama' => 'Perencanaan dan Monev Program dan Kegiatan'
+        ]);
+
+        Departemen::create([
+            'nama' => 'Keuangan DIPA'
+        ]);
+
+        Departemen::create([
+            'nama' => 'Keuangan Non DIPA'
+        ]);
+
+        Departemen::create([
+            'nama' => 'Pemeliharaan Gedung dan Halaman Gedung'
+        ]);
+
+        Departemen::create([
+            'nama' => 'Pemeliharaan Alat dan Mesin'
         ]);
     }
 }
